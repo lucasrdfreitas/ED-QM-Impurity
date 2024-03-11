ed6536316e8eb395bce9d11f3fc6f251e113623d
lucasrdfreitas <lucasrdf@fisica.ufrn.br>
2024-03-11 14:52:40 -0300

    sub33 simp=1/2 script

---
+++ script33sub.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+#$ -l hostname=micro4.local 
+#$ -N sub33_S.5
+#$ -l h_rt=180:00:00                   #estimate max run time
+#$ -q all.q
+#$ -m ea
+#$ -M lucasrdf@fisica.ufrn.br
+#$ -cwd
+#$ -o /home/rodrigues/ED-QM-Impurity/out
+#$ -e /home/rodrigues/ED-QM-Impurity/err
+#
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/sge/lib/lx-amd64
+export CLASSPATH=/opt/sge/lib/drmaa.jar:/opt/sge/lib/juti.jar:/opt/sge/lib/jgdi.jar
+#
+math -script substitution33.m
